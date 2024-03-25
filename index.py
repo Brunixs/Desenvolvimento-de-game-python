2d0738248edbe1d18f5999e83834759033e852c6
Brunixs <112876996+Brunixs@users.noreply.github.com>
2024-03-25 15:45:43 +0000

    Criação das listas e criação de loops e condições

--- index.py
+++ index.py
@@ -22,3 +22,41 @@ def game():
     # Lista de Palavras 
     palavras = ['carro', 'moto', 'uva', 'peixe', 'humano', 'livros', 'banana', 'macaco', 'escada', 'sol', 'lua', 'refrigerante', 'nave', 'gato', 'cachorro', 'hamster']
 
+    # Escolhendo uma palavra aleatoria
+    palavra = random.choice(palavras)
+    
+    # list comprehension (Retorna '_' para a cada letra da palavra aleatoria)
+    letra_descobertas = ['_' for letra in palavra]
+
+    # numero de chances
+    chances = 6
+
+    # lista para armazenar letras erradas
+    letra_errada = []
+
+    # loop enquanto o valor de chances for maior que 0
+    while chances > 0:
+        print(' '.join(letra_descobertas))
+        print("Chances Restantes:", chances)
+        print("Letras Errads:", " ".join(letra_errada))
+
+        # tentativa
+        tentativa = input("Digite uma letra: ").lower()
+
+        # Condicional
+        if tentativa in palavra:
+            index = 0
+            for letra in palavra:
+                if tentativa == letra:
+                    letra_descobertas[index] = letra
+                index += 1
+        else:
+            chances -= 1
+            letra_errada.append(tentativa)
+
+            
+
+
+
+
+game()
